ecf00ce3a2d4546f38ac3c98bf338e7fd4bfed73
jasmyneannagica <anyka.jasmyne2@gmail.com>
2024-11-28 18:07:14 -0300

    atualizado por Anna

--- JAVASCRIPT/empório.js
+++ JAVASCRIPT/empório.js
@@ -1,24 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDuV8qqdrSSg3ytl6tHcxRWMFgXduBpZSg",
-  authDomain: "tcc245-1aecf.firebaseapp.com",
-  projectId: "tcc245-1aecf",
-  storageBucket: "tcc245-1aecf.firebasestorage.app",
-  messagingSenderId: "534494847982",
-  appId: "1:534494847982:web:9e4348a2fd3e219fbf64ff",
-  measurementId: "G-2KJG23CYRD"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 
 
